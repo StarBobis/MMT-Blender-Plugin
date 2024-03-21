47a23c662fccce67bbc60f3d789bbed55223bc02
NicoMico <Administrator@DESKTOP>
2024-03-21 08:31:48 +0800

    添加快速导入导出

--- Blender3DmigotoLight/utils.py
+++ Blender3DmigotoLight/utils.py
@@ -30,6 +30,7 @@ from mathutils import Matrix, Vector
 from bpy_extras.io_utils import orientation_helper
 
 import logging
+import json
 
 
 class MMTPathProperties(bpy.types.PropertyGroup):
@@ -46,19 +47,9 @@ class MMTPathOperator(bpy.types.Operator):
     bl_label = "Select Folder"
 
     def execute(self, context):
-        bpy.ops.wm.console_toggle()  # 打开控制台面板
         # 在这里处理文件夹选择逻辑
         bpy.ops.ui.directory_dialog('INVOKE_DEFAULT', directory=context.scene.mmt_props.path)
 
-        print(context.scene.mmt_props.path)
-        mmt_path = os.path.join(context.scene.mmt_props.path, "MMT.exe")
-        # TODO 在这里读取配置文件，并设置outputFolder的路径为当前选择游戏的路径。设置当前游戏的提示，设置MMT path
-        if os.path.exists(mmt_path):
-            print("MMT.exe exists.")
-        else:
-            print(mmt_path)
-
-        bpy.ops.wm.console_toggle()  # 关闭控制台面板
         return {'FINISHED'}
 
 
@@ -74,37 +65,68 @@ class MMTPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
+        row = layout.row()
         # 在这里添加你的侧边栏内容
-        layout.label(text="Thanks for using MMT Plugin! Happy Day!")
+        row.label(text="NicoMico's MMT Plugin V1.4")
+
+        row.operator("wm.url_open", text="Check Update", icon='URL').url = "https://github.com/StarBobis/3Dmigoto-Blender-MMT/releases"
 
         props = context.scene.mmt_props
         layout.prop(props, "path")
 
-        layout.separator()
-        layout.label(text="Current working game: HI3")
+        # 获取MMT.exe的路径
+        mmt_path = os.path.join(context.scene.mmt_props.path, "MMT.exe")
+        if os.path.exists(mmt_path):
+            layout.label(text="MMT: " + mmt_path)
+        else:
+            layout.label(text="Error: Invalid MMT path! ", icon='ERROR')
+
+        # 读取MainSetting.json中当前游戏名称
+        current_game = ""
+        main_setting_path = os.path.join(context.scene.mmt_props.path, "Configs\\wheel_setting\\MainSetting.json")
+        if os.path.exists(main_setting_path):
+            main_setting_file = open(main_setting_path)
+            main_setting_json = json.load(main_setting_file)
+            main_setting_file.close()
+            current_game = main_setting_json["GameName"]
+            layout.label(text="Working game: " + current_game)
+        else:
+            layout.label(text="Error: Can't find MainSetting.json! ", icon='ERROR')
+
+        # 根据当前游戏名称，读取GameSetting中的OutputFolder路径并设置
+        output_folder_path = ""
+        game_setting_path = os.path.join(context.scene.mmt_props.path, "Configs\\wheel_setting\\GameSetting.json")
+        if os.path.exists(game_setting_path):
+            game_setting_file = open(game_setting_path)
+            game_setting_json = json.load(game_setting_file)
+            game_setting_file.close()
+            output_folder_path = game_setting_json[current_game + "_Dev"]["OutputFolder"]
+            layout.label(text="OutputFolder: " + output_folder_path)
+        else:
+            layout.label(text="Error: Can't find GameSetting.json! ", icon='ERROR')
 
         layout.separator()
         layout.label(text="Seperate import&export in OutputFolder")
 
-        # TODO 快速导入，点这个之后默认路径为OutputFolder，这样直接就能去导入不用翻很久文件夹找路径了
+        # 快速导入，点这个之后默认路径为OutputFolder，这样直接就能去导入不用翻很久文件夹找路径了
         operator_import_txt = self.layout.operator("import_mesh.migoto_frame_analysis", text="Import .txt models ")
-        operator_import_txt.directory = props.path
+        operator_import_txt.directory = output_folder_path
 
-        # TODO 快速导出同理，点这个之后默认路径为OutputFolder，这样直接就能去导出不用翻很久文件夹找路径了
+        # 快速导出同理，点这个之后默认路径为OutputFolder，这样直接就能去导出不用翻很久文件夹找路径了
         operator_export_ibvb = self.layout.operator("export_mesh.migoto", text="Export .ib & .vb files ")
-        operator_export_ibvb.filepath = context.scene.mmt_props.path + "1.vb"
+        operator_export_ibvb.filepath = output_folder_path + "1.vb"
 
         # 添加分隔符
         layout.separator()
 
         # TODO 一键快速导入所有位于OutputFolder下的.txt模型
-        layout.label(text="Fast import&export in OutputFolder")
-        operator44 = self.layout.operator("import_mesh.migoto_frame_analysis", text="Import All .txt model in OutputFolder ")
-        operator44.filepath = context.scene.mmt_props.path + "1.vb"
+        # layout.label(text="Fast import&export in OutputFolder")
+        # operator44 = self.layout.operator("import_mesh.migoto_frame_analysis", text="Import All .txt model in OutputFolder ")
+        # operator44.filepath = context.scene.mmt_props.path + "1.vb"
 
         # TODO 一键快速导出当前选中Collection中的所有model到对应的hash值文件夹中，并直接调用MMT.exe的Mod生成方法，做到导出完即可游戏里F10刷新看效果。
-        operator_export_ibvb = self.layout.operator("export_mesh.migoto", text="Export selected collection to generated mod folder ")
-        operator_export_ibvb.filepath = context.scene.mmt_props.path + "1.vb"
+        # operator_export_ibvb = self.layout.operator("export_mesh.migoto", text="Export selected collection to generated mod folder ")
+        # operator_export_ibvb.filepath = context.scene.mmt_props.path + "1.vb"
 
 
 
