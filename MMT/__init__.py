eac6c39d1d16dbca16970fad72f6e20267299786
NicoMico <Administrator@DESKTOP>
2024-06-10 12:33:03 +0800

    upgrade to 1.5.0

--- MMT/__init__.py
+++ MMT/__init__.py
@@ -15,7 +15,7 @@ bl_info = {
     "author": "NicoMico",
     "description": "Special fork version of DarkStarSword's blender_3dmigoto.py",
     "blender": (3, 6, 8),
-    "version": (1, 4, 9),
+    "version": (1, 5, 0),
     "location": "View3D",
     "warning": "",
     "category": "Generic"
